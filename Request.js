12270b08ae15db15ee0cc23142c5deaf7bcf3af5
hc33brackles <47486204+hc33brackles@users.noreply.github.com>
2021-11-10 14:25:08 -0800

    Update Request.js
    replaced my email on line 7 with <youremail>

--- Request.js
+++ Request.js
@@ -4,7 +4,7 @@ _EXTERNAL_URL = 'https://api.weather.gov/gridpoints/TOP/31,80/forecast'
 
 const callExternalApiUsingRequest = (callback) => {
 //This is the line that contains the User-Agent Header. The weather.gov api asks you to put your own <unique identifier> so that they can monitor for excessive use
-    request(_EXTERNAL_URL, {headers: {'User-Agent': '<uniqueID>: (Anant, nicholas.brackley@anant.us)'}, json: true }, (err, res, body) => {
+    request(_EXTERNAL_URL, {headers: {'User-Agent': '<uniqueID>: (Anant, <youremail>)'}, json: true }, (err, res, body) => {
     if (err) { 
         return callback(err);
      }
